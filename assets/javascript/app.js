317d2815c85dbff4214441807358915ee24ae1ba
Evgenia Semez <esemez@indeed.com>
2019-04-09 16:53:33 -0700

    Adding logic to the project and polishing user input

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -28,12 +28,35 @@ $("#submitButton").on("click", function (event) {
     console.log(firstTrainTime);
     console.log(frequency);
 
+    // evaluate input
+    var regex = /^[a-z0-9 ]+$/i;
+    if (!trainName.match(regex)) {
+        $("#trainName").val("");
+        return;
+    }
+
+    if (!trainDestination.match(regex)) {
+        $("#destination").val("");
+        return;
+    }
+
+    if (!moment(firstTrainTime, "HH:mm").isValid()) {
+        $("#firstTrainTime").val("");
+        return;
+    }
+    if (isNaN(frequency)) {
+        $("#frequency").val("");
+        return;
+    }
+
     database.ref(trainData).push({
         trainWay: trainName,
         destination: trainDestination,
         trainTime: firstTrainTime,
         trainfrequency: frequency
     });
+
+
     $("#trainName").val("");
     $("#destination").val("");
     $("#firstTrainTime").val("");
@@ -48,26 +71,40 @@ trainDataRef.on("value", function (snap) {
     $("tbody").html("");
     snap.forEach(function (childSnapshot) {
 
-
         var firstTrainTime = moment(childSnapshot.val().trainTime, "HH:mm");
         var trainFrequency = childSnapshot.val().trainfrequency;
         var n;
-        n = Math.ceil(nowTime.diff(firstTrainTime, "minutes") / parseFloat(trainFrequency));
+        var minutesAway;
+        var nextTrainTime;
 
-
-        var minutesAway = trainFrequency * n - (nowTime.diff(firstTrainTime, "minutes"));
-
-        var nextTrainTime = (nowTime.add(minutesAway, "minutes"));
+        console.log(nowTime.format());
+        console.log(firstTrainTime.format());
+        console.log(nowTime.isBefore(firstTrainTime));
+        if (nowTime.isBefore(firstTrainTime)) {
+            console.log("Future start time");
+            minutesAway = firstTrainTime.diff(nowTime, "minutes");
+            // nextTrainTime = (nowTime.add(minutesAway, "minutes"));
+            nextTrainTime = firstTrainTime;
+        }
+        else {
+            console.log("Past start time");
+            n = Math.ceil(nowTime.diff(firstTrainTime, "minutes") / parseFloat(trainFrequency));
+            minutesAway = trainFrequency * n - (nowTime.diff(firstTrainTime, "minutes"));
+            // nextTrainTime = (nowTime.add(minutesAway, "minutes"));
+            var nowClone = moment(nowTime);
+            nowClone.add(minutesAway);
+            nextTrainTime = nowClone;
+        }
 
 
         var row = $("<tr>");
         row.append("<td>" + childSnapshot.val().trainWay + "</td>");
         row.append("<td>" + childSnapshot.val().destination + "</td>");
         row.append("<td>" + childSnapshot.val().trainfrequency + "</td>");
-        row.append("<td>" + nextTrainTime.format("HH:mm") + "</td>");
+        row.append("<td>" + nextTrainTime.format("LT") + "</td>");
         row.append("<td>" + minutesAway + "</td>");
         $("tbody").append(row);
-        
+
 
     });
 });
